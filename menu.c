c6d78f7d69c6f81e6b00cb5b180d5ca026c2e227
Daniel Porto <86478693+danportoo@users.noreply.github.com>
2021-11-09 13:21:07 -0300

    adição dos cursos e do login de admin

--- menu.c
+++ menu.c
@@ -183,7 +183,7 @@ void curso3() //Aqui é o menu do curso 3
 	
     {
          system("cls");
-         printf("\n|--------------------------------------|\n"); // Menu CURSO 2
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 3
          printf("|               DEVOPS                 |\n");
          printf("|--------------------------------------|\n");
          printf("|                                      |\n");
@@ -204,13 +204,13 @@ void curso3() //Aqui é o menu do curso 3
                 system("cls");
                 main();
             }
-          while (opcao != 1 && opcao != 2 && opcao!=0) // tratando erro (menu CURSO 2)
+          while (opcao != 1 && opcao != 2 && opcao!=0) // tratando erro (menu CURSO 3)
         {
          system("cls");
-         printf("\n|--------------------------------------|\n"); // Menu CURSO 2
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 3
          printf("|               DEVOPS                 |\n");
          printf("|--------------------------------------|\n");
-         printf("|           OPECAO INVALIDA            |\n");
+         printf("|           OPCAO INVALIDA            |\n");
          printf("|                                      |\n");
          printf("|          CONTEUDO DO CURSO           |\n");
          printf("|                                      |\n");
@@ -230,5 +230,306 @@ void curso3() //Aqui é o menu do curso 3
             }
         }
     }
+}
+
+void curso4() //Aqui é o menu do curso 4
+{
+    {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 4
+         printf("|               MOBILE                 |\n");
+         printf("|--------------------------------------|\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * React Native               |\n");
+         printf("|         * Flutter                    |\n"); 
+         printf("|         * Kotlin                     |\n");
+         printf("|         * Ios e Swift                |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+        scanf("%d", &opcao);
+
+          if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+          while (opcao != 1 && opcao != 2 && opcao!=0) // tratando erro (menu CURSO 4)
+        {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 4
+         printf("|               MOBILE                 |\n");
+         printf("|--------------------------------------|\n");
+         printf("|           OPCAO INVALIDA            |\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * React Native               |\n");
+         printf("|         * Flutter                    |\n"); 
+         printf("|         * Kotlin                     |\n");
+         printf("|         * Ios e Swift                |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+             scanf("%d", &opcao);
+             if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+        }
+    }
+}
+
+void curso5() //Aqui é o menu do curso 5
+{
+    {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 5
+         printf("|             DATA SCIENCE             |\n");
+         printf("|--------------------------------------|\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * Ciencia de dados           |\n");
+         printf("|         * BI                         |\n"); 
+         printf("|         * SQL                        |\n");
+         printf("|         * Banco de dados             |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+        scanf("%d", &opcao);
+
+          if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+          while (opcao != 1 && opcao != 2 && opcao!=0) // tratando erro (menu CURSO 5)
+        {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 5
+         printf("|             DATA SCIENCE             |\n");
+         printf("|--------------------------------------|\n");
+         printf("|           OPCAO INVALIDA            |\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * Ciencia de dados           |\n");
+         printf("|         * BI                         |\n"); 
+         printf("|         * SQL                        |\n");
+         printf("|         * Banco de dados             |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+             scanf("%d", &opcao);
+             if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+        }
+    }
+}
+
+void curso6() //Aqui é o menu do curso 6
+
+{
+    {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 6
+         printf("|              UX & DESING             |\n");
+         printf("|--------------------------------------|\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * Photoshop e Illustrator    |\n");
+         printf("|         * Usabilidade e UX           |\n"); 
+         printf("|         * Video e Motion             |\n");
+         printf("|         * 3D                         |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+        scanf("%d", &opcao);
+
+          if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+          while (opcao != 1 && opcao != 2 && opcao!=0) // tratando erro (menu CURSO 6)
+        {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 6
+         printf("|              UX & DESING             |\n");
+         printf("|--------------------------------------|\n");
+         printf("|           OPCAO INVALIDA            |\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * Photoshop e Illustrator    |\n");
+         printf("|         * Usabilidade e UX           |\n"); 
+         printf("|         * Video e Motion             |\n");
+         printf("|         * 3D                         |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+             scanf("%d", &opcao);
+             if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+        }
+    }
+}
+
+void curso7() //Aqui é o menu do curso 7
+
+{
+    {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 7
+         printf("|           INOVACAO & GESTAO          |\n");
+         printf("|--------------------------------------|\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * Metodologias Ageis         |\n");
+         printf("|         * Softskills                 |\n"); 
+         printf("|         * Liderança e Gestao         |\n");
+         printf("|         * Startups                   |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+        scanf("%d", &opcao);
+
+          if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+          while (opcao != 1 && opcao != 2 && opcao!=0) // tratando erro (menu CURSO 7)
+        {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 7
+         printf("|           INOVACAO & GESTAO          |\n");
+         printf("|--------------------------------------|\n");
+         printf("|           OPCAO INVALIDA             |\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * Métodologias Ágeis         |\n");
+         printf("|         * Softskills                 |\n"); 
+         printf("|         * Liderança e Gestão         |\n");
+         printf("|         * Startups                   |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+             scanf("%d", &opcao);
+             if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+        }
+    }
+}
+
+void curso8() //Aqui é o menu do curso 8
+
+{
+    {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 7
+         printf("|        SEGURANÇA DA INFORMACAO       |\n");
+         printf("|--------------------------------------|\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * LGPD                       |\n");
+         printf("|         * Redes e Firewall           |\n"); 
+         printf("|         * GRC                        |\n");
+         printf("|         * ISO 27001                  |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+        scanf("%d", &opcao);
+
+          if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+          while (opcao != 1 && opcao != 2 && opcao!=0) // tratando erro (menu CURSO 7)
+        {
+         system("cls");
+         printf("\n|--------------------------------------|\n"); // Menu CURSO 7
+         printf("|        SEGURANÇA DA INFORMACAO       |\n");
+         printf("|--------------------------------------|\n");
+         printf("|           OPCAO INVALIDA            |\n");
+         printf("|                                      |\n");
+         printf("|          CONTEUDO DO CURSO           |\n");
+         printf("|                                      |\n");
+         printf("|         * LGPD                       |\n");
+         printf("|         * Redes e Firewall           |\n"); 
+         printf("|         * GRC                        |\n");
+         printf("|         * ISO 27001                  |\n");
+         printf("|--------------------------------------|\n");
+         printf("| 1-COMPRAR 2-CURSOS 0-MENU PRINCIPAL  |\n");
+         printf("|--------------------------------------|\n");
+         
+             scanf("%d", &opcao);
+             if (opcao ==0)
+            {
+                system("cls");
+                main();
+            }
+        }
+    }
+}
+
+void telaLogin() // função de tela de login para entrar no admin do site//
+{
+
+    char login[15] = "teste";
+    char login1[15];
+    char senha[15] = "teste";
+    char senha1[15];        
+    int login_efetuado = 0; //0 - Falso e  1 - Verdadeiro
+
+    while(!login_efetuado)
+    {
+        printf("Digite o Login: ");
+        scanf("%s", login1);
+
+        printf("Digite a Senha: ");
+        scanf("%s", senha1);
+
+        if (strcmp(login, login1) == 0 && strcmp(senha, senha1) == 0)
+        {
+            printf("\n\nLOGADO!\n\n");
+            login_efetuado = 1;
+        }
+        else
+            printf("\n\nDADOS INVALIDOS!\n\n");    
+    
+        if (login_efetuado=1)
+        {
+            //menu de admin do curso//             
+        }
+    }
+
 }
 //fim da função
